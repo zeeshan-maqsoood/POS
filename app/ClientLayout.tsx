0e92135120223d789dfa818b930ea3d08f5e8bcd
zeeshan-maqsoood <zeeshanmaqsood129@gmail.com>
2025-11-11 17:05:34 +0500

    add print Node functionality

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -7,18 +7,15 @@ import { Toaster } from 'sonner';
 import { PermissionsProvider } from '@/contexts/PermissionsContext';
 import { SocketProvider } from '@/contexts/SocketContext';
 import { OrderNotifications } from '@/components/notifications/OrderNotifications';
-import { QZTrayProvider } from '@/components/qztray/QZTrayProvider';
 
 export default function ClientLayout({ children }: { children: ReactNode }) {
   return (
     <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading...</div>}>
       <SocketProvider>
         <PermissionsProvider>
-          <QZTrayProvider>
-            {children}
-            <OrderNotifications />
-            <Toaster position="top-center" />
-          </QZTrayProvider>
+          {children}
+          <OrderNotifications />
+          <Toaster position="top-center" />
         </PermissionsProvider>
       </SocketProvider>
     </Suspense>
